21a06c5ed85c4653c6d9f0f8495d96cffc2f68fb
Syedmansoor7866 <112846618+Syedmansoor7866@users.noreply.github.com>
2022-09-08 09:15:57 +0530

    Add files via upload

---
+++ test-5(8-9-22)2ndquestionarea.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+class area
+{
+	public:
+		void rectangle(int a,int b)
+		{
+			cout<<"area of rectangle:"<<endl<<a*b;
+		}
+		void square(int a)
+		{
+			cout<<"area of square:"<<endl<<a*a<<endl;
+		}
+};
+int main()
+{
+	area obj1;
+	obj1.rectangle(6,8);
+	obj1.square(8);
+	return 0;
+}
